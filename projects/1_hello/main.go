b983d3fb4522a786f5c1ebbc7c845d7d98ee3616
Nikita <nmamaev49@gmail.com>
2024-11-16 00:57:55 +0300

    lw7

--- projects/1_hello/main.go
+++ projects/1_hello/main.go
@@ -1,7 +1,23 @@
 package main
 
-// здесь надо написать код
+// некоторые импорты нужны для проверки
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	// и здесь тоже
+	// Регистрируем обработчики для разных путей
+	http.HandleFunc("/", handler)
+
+	// Запускаем веб-сервер на порту 8080
+	err := http.ListenAndServe(":8082", nil)
+	if err != nil {
+		fmt.Println("Ошибка запуска сервера:", err)
+	}
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Write([]byte("Hello, web!"))
 }
